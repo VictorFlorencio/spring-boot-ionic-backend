f5b582836a0f6ef00f66a2b848f2dfb5c9d55afd
Victor Florencio <victor.florencio01@gmail.com>
2018-07-01 11:22:55 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -18,10 +18,11 @@ public class ItemPedido implements Serializable {
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
-	public ItemPedido() {}
 
-	public ItemPedido(Pedido pedido, Produto produto,  Double desconto, Integer quantidade, Double preco) {
+	public ItemPedido() {
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
@@ -29,26 +30,31 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
-		
 	}
-	
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 	public Produto getProduto() {
 		return id.getProduto();
 	}
 
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
 	public ItemPedidoPK getId() {
 		return id;
 	}
 
+	
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
@@ -102,5 +108,4 @@ public class ItemPedido implements Serializable {
 		return true;
 	}
 
-  
 }
